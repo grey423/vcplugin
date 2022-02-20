7c9fce586a40dda992f076079cb4d4a6d5a8931d
Grey Project <95908917+grey423@users.noreply.github.com>
2022-02-20 09:11:04 +0700

    Initial commit

---
+++ userbot.py
@@ -0,0 +1,23 @@
+# import logging
+from pyrogram import Client, idle
+
+API_ID = int(environ["API_ID"])
+API_HASH = environ["API_HASH"]
+SESSION_NAME = environ["SESSION_NAME"]
+
+PLUGINS = dict(
+    root="plugins",
+    include=[
+        "vc." + environ["PLUGIN"],
+        "ping",
+        "sysinfo"
+    ]
+)
+
+app = Client(SESSION_NAME, API_ID, API_HASH, plugins=PLUGINS)
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> USERBOT STARTED')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED')
